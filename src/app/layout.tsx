583c9ec74d9eab7c7a5e0e9588e385b39e7ad0a5
kabeerx9 <kabeer786joshi@gmail.com>
2023-12-20 16:24:08 +0530

    Prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,41 +6,42 @@ import { ThemeProvider } from '@/components/Providers/theme-provider';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-	title: 'Jotion',
-	description: 'The connected workspace where better , faster work happens',
-	icons: {
-		icon: [
-			{
-				media: '(prefers-color-scheme:light)',
-				url: '/logo.svg',
-				href: '/logo.svg',
-			},
-			{
-				media: '(prefers-color-scheme:dark)',
-				url: '/logo-dark.svg',
-				href: '/logo-dark.svg',
-			},
-		],
-	},
+  title: 'Jotion',
+  description: 'The connected workspace where better , faster work happens',
+  icons: {
+    icon: [
+      {
+        media: '(prefers-color-scheme:light)',
+        url: '/logo.svg',
+        href: '/logo.svg',
+      },
+      {
+        media: '(prefers-color-scheme:dark)',
+        url: '/logo-dark.svg',
+        href: '/logo-dark.svg',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
-	children,
+  children,
 }: {
-	children: React.ReactNode;
+  children: React.ReactNode;
 }) {
-	return (
-		<html lang="en" suppressHydrationWarning>
-			<body className={inter.className}>
-				<ThemeProvider
-					attribute="class"
-					defaultTheme="system"
-					enableSystem
-					disableTransitionOnChange
-					storageKey="jotion-theme">
-					{children}
-				</ThemeProvider>
-			</body>
-		</html>
-	);
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+          storageKey="jotion-theme"
+        >
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }

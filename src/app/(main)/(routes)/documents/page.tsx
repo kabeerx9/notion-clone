cee5769a892933bbec20ce3225294450ab8e046e
kabeerx9 <kabeer786joshi@gmail.com>
2023-12-21 18:54:19 +0530

    First note created using convex functions

--- src/app/(main)/(routes)/documents/page.tsx
+++ src/app/(main)/(routes)/documents/page.tsx
@@ -4,9 +4,24 @@ import Image from 'next/image';
 import { useUser } from '@clerk/clerk-react';
 import { Button } from '@/components/ui/button';
 import { PlusCircle } from 'lucide-react';
+import { useMutation } from 'convex/react';
+import { api } from '../../../../../convex/_generated/api';
+import { toast } from 'sonner';
 
 const DocumentsPage = () => {
   const { user } = useUser();
+  const create = useMutation(api.documents.create);
+
+  const onCreate = () => {
+    const promise = create({
+      title: 'Untitled',
+    });
+    toast.promise(promise, {
+      loading: 'Creating document...',
+      success: 'Document created!',
+      error: 'Failed to create document',
+    });
+  };
 
   return (
     <div className="h-full flex flex-col items-center justify-center space-y-4">
@@ -27,7 +42,7 @@ const DocumentsPage = () => {
       <h2 className="text-lg font-medium">
         Welcome to {user?.firstName}&apos;s Jotion
       </h2>
-      <Button>
+      <Button onClick={onCreate}>
         <PlusCircle className="h-6 w-6 mr-2" />
         Create a note
       </Button>

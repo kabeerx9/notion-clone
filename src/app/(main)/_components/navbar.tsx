008b3f3a7b496c187d24112947011fa265d2679f
kabeerx9 <kabeer786joshi@gmail.com>
2023-12-24 18:23:23 +0530

    Banner

--- src/app/(main)/_components/navbar.tsx
+++ src/app/(main)/_components/navbar.tsx
@@ -5,7 +5,9 @@ import { useParams } from 'next/navigation';
 import { api } from '../../../../convex/_generated/api';
 import { Id } from '../../../../convex/_generated/dataModel';
 import { MenuIcon } from 'lucide-react';
+import Menu from './menu';
 import Title from './title';
+import Banner from './banner';
 
 interface NavbarProps {
   isCollapsed: boolean;
@@ -21,8 +23,11 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
 
   if (document === undefined) {
     return (
-      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center">
+      <nav className="bg-background dark:bg-[#1F1F1F] px-3 py-2 w-full flex items-center justify-between">
         <Title.Skeleton />
+        <div className="flex items-center gap-x-2">
+          <Menu.Skeleton />
+        </div>
       </nav>
     );
   }
@@ -43,8 +48,12 @@ const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
         )}
         <div className="flex items-center justify-between w-full">
           <Title initialData={document} />
+          <div className="flex items-center gap-x-2">
+            <Menu documentId={document._id} />
+          </div>
         </div>
       </nav>
+      {document.isArchived && <Banner documentId={document._id} />}
     </>
   );
 };
